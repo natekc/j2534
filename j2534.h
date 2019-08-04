eecebe759fe002be4d757a6ec627a5876222139d
Nathan Chan <nathankcchan@gmail.com>
2019-08-04 12:38:30 -0700

    Improve readability for header file

--- j2534.h
+++ j2534.h
@@ -36,18 +36,41 @@ typedef struct _ConnectionStruct
 
 long PassThruOpen(const void* pName, unsigned long* pDeviceID);
 long PassThruClose(unsigned long DeviceID);
-long PassThruConnect(unsigned long DeviceID, unsigned long ProtocolID, unsigned long Flags,
-					unsigned long Baudrate, unsigned long* pChannelID);
+long PassThruConnect(unsigned long DeviceID,
+                     unsigned long ProtocolID,
+                     unsigned long Flags,
+					           unsigned long Baudrate,
+                     unsigned long* pChannelID);
 long PassThruDisconnect(unsigned long ChannelID);
-long PassThruReadMsgs(unsigned long ChannelID, PASSTHRU_MSG* pMsg, unsigned long* pNumMsgs, unsigned long Timeout);
-long PassThruWriteMsgs(unsigned long ChannelID, const PASSTHRU_MSG* pMsg, unsigned long* pNumMsgs, unsigned long Timeout);
-long PassThruStartPeriodicMsg(unsigned long ChannelID, const PASSTHRU_MSG* pMsg, unsigned long* pMsgID, unsigned long TimeInterval);
+long PassThruReadMsgs(unsigned long ChannelID,
+                      PASSTHRU_MSG* pMsg,
+                      unsigned long* pNumMsgs,
+                      unsigned long Timeout);
+long PassThruWriteMsgs(unsigned long ChannelID,
+                       const PASSTHRU_MSG* pMsg,
+                       unsigned long* pNumMsgs,
+                       unsigned long Timeout);
+long PassThruStartPeriodicMsg(unsigned long ChannelID,
+                              const PASSTHRU_MSG* pMsg,
+                              unsigned long* pMsgID,
+                              unsigned long TimeInterval);
 long PassThruStopPeriodicMsg(unsigned long ChannelID, unsigned long MsgID);
 long PassThruStartMsgFilter(unsigned long ChannelID,
-                      unsigned long FilterType, const PASSTHRU_MSG* pMaskMsg, const PASSTHRU_MSG* pPatternMsg,
-                      const PASSTHRU_MSG* pFlowControlMsg, unsigned long* pMsgID);
+                            unsigned long FilterType,
+                            const PASSTHRU_MSG* pMaskMsg,
+                            const PASSTHRU_MSG* pPatternMsg,
+                            const PASSTHRU_MSG* pFlowControlMsg,
+                            unsigned long* pMsgID);
 long PassThruStopMsgFilter(unsigned long ChannelID, unsigned long MsgID);
-long PassThruSetProgrammingVoltage(unsigned long DeviceID, unsigned long Pin, unsigned long Voltage);
-long PassThruReadVersion(unsigned long DeviceID, char* pApiVersion, char* pDllVersion, char* pFirmwareVersion);
+long PassThruSetProgrammingVoltage(unsigned long DeviceID,
+                                   unsigned long Pin,
+                                   unsigned long Voltage);
+long PassThruReadVersion(unsigned long DeviceID,
+                         char* pApiVersion,
+                         char* pDllVersion,
+                         char* pFirmwareVersion);
 long PassThruGetLastError(char* pErrorDescription);
-long PassThruIoctl(unsigned long ChannelID, unsigned long IoctlID, const void* pInput, void* pOutput);
+long PassThruIoctl(unsigned long ChannelID,
+                   unsigned long IoctlID,
+                   const void* pInput,
+                   void* pOutput);
